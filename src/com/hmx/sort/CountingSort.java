a4672673642ee4f5baa1fab31896e9775393f6f3
hmx-coder <635137843@qq.com>
2020-08-07 15:12:20 +0800

    计数排序

--- src/com/hmx/sort/CountingSort.java
+++ src/com/hmx/sort/CountingSort.java
@@ -8,8 +8,48 @@ package com.hmx.sort;
  **/
 public class CountingSort extends Sort<Integer> {
 
+
     @Override
     protected void sort() {
+        //找出最值
+        int max = array[0];
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > max){
+                max = array[i];
+            }
+            if(array[i] < min){
+                min = array[i];
+            }
+        }
+
+        //开辟空间，存储次数
+        int[] counts = new int[max - min + 1];
+
+        //根据新的统计表获得索引
+        for (int i = 0; i < array.length; i++) {
+            counts[array[i] - min]++;
+        }
+
+        //累加次数
+        for (int i = 1; i < counts.length; i++) {
+            counts[i] += counts[ i - 1];
+        }
+
+        //从后往前便利元素，让其放入有序数组的适应位置
+        int[] newArray = new int[array.length];
+        //减去数组中最小的元素找到对应索引然后-1就是适合位置
+        for (int i = array.length - 1; i >= 0; i--) {
+            newArray[--counts[array[i] - min]] = array[i];
+        }
+
+        //将有序数据复制到array
+        for (int i = 0; i < newArray.length; i++) {
+            array[i]  = newArray[i];
+        }
+    }
+
+    private void sort0(){
         //找出最大值确定数组长度
         int max = array[0];
         for (int i = 0; i < array.length; i++) {
@@ -35,5 +75,71 @@ public class CountingSort extends Sort<Integer> {
         }
     }
 
+    public static void main(String[] args){
+        Person[] people = new Person[]{
+                new Person(20,"A"),
+                new Person(-20,"B"),
+                new Person(13,"C"),
+                new Person(-14,"D"),
+                new Person(-14,"E"),
+                new Person(30,"F"),
+        };
 
+        //找出最值
+        int max = people[0].age;
+        int min = people[0].age;
+        for (int i = 0; i < people.length; i++) {
+            if(people[i].age > max){
+                max = people[i].age;
+            }
+            if(people[i].age < min){
+                min = people[i].age;
+            }
+        }
+
+        //开辟空间，存储次数
+        int[] counts = new int[max - min + 1];
+
+        //根据新的统计表获得索引
+        for (int i = 0; i < people.length; i++) {
+            counts[people[i].age - min]++;
+        }
+
+        //累加次数
+        for (int i = 1; i < counts.length; i++) {
+            counts[i] += counts[ i - 1];
+        }
+
+        //从后往前便利元素，让其放入有序数组的适应位置
+        Person[] newArray = new Person[people.length];
+        //减去数组中最小的元素找到对应索引然后-1就是适合位置
+        for (int i = people.length - 1; i >= 0; i--) {
+            newArray[--counts[people[i].age - min]] = people[i];
+        }
+
+        //将有序数据复制到array
+        for (int i = 0; i < newArray.length; i++) {
+            people[i]  = newArray[i];
+        }
+
+        for (int i = 0; i < people.length; i++) {
+            System.out.println(people[i]);
+        }
+    }
+
+    private static class Person{
+        int age;
+        String name;
+        Person(int age, String name){
+            this.age = age;
+            this.name = name;
+        }
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "age=" + age +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
 }
---
+++ src/com/hmx/sort/RadixSort.java
@@ -0,0 +1,10 @@
+package com.hmx.sort;
+
+/**
+ * @program: eoms-workflow
+ * @description:
+ * @author: HeMingXin
+ * @create: 2020/8/7 15:11
+ **/
+public class RadixSort {
+}
